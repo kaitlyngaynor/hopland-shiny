4d152c22b38b57e7aa03bcc8f9050a9f1ad71079
Kaitlyn Gaynor <gaynor@nceas.ucsb.edu>
2020-02-27 14:46:32 -0800

    Adapt data cleaning file for Hopland

--- 01-clean-for-shiny.R
+++ 01-clean-for-shiny.R
@@ -6,30 +6,23 @@ library(tidyverse)
 
 # Import and merge record tables ---------------------------------------------
 
-records1 <- read_csv("~/Dropbox/projects/Hopland - Kaitlyn dissertation/R/hopland-camera-traps/data/recordtable_phase1_cleaned30sec.csv")
-records2 <- read_csv("~/Dropbox/projects/Hopland - Kaitlyn dissertation/R/hopland-camera-traps/data/recordtable_phase2_cleaned30sec.csv")
-records <- rbind(records1, records2)
-
+# just phase 1 for now
+records <- read_csv("~/Dropbox/projects/Hopland - Kaitlyn dissertation/R/hopland-camera-traps/data/recordtable_phase1_cleaned30sec.csv")
 
 # Fix deer classifications ------------------------------------------------
 
-## see code in 02-clean-deer-records.Rmd
-
-
-
-
-
+# actually going to do this in the server... for now, "Species" column is 'Deer" and there are other columns for sex, age, buck class
 
 # Format and scale times ------------------------------------------------------------
 
 # set spatial coordinates
-coords <- matrix(c(34.50, -18.82), nrow=1) %>%
+coords <- matrix(c(-123.07, 39.00), nrow=1) %>%
   sp::SpatialPoints(proj4string=sp::CRS("+proj=longlat +datum=WGS84"))
 
 # specify date format
 records$Date <- as.POSIXct(records$Date, 
                            format = "%m/%d/%y",
-                           tz = "Africa/Maputo")
+                           tz = "North_America/Los_Angeles")
 
 # convert time to radians (could be done more efficiently with pipe)
 records$Time.Corrected <- hms(records$Time)
@@ -42,10 +35,13 @@ records$Time.Sun <- sunTime(records$Time.Radians, records$Date, coords)
 
 # Drop records outside of operation dates ------------------------------------------------------------
 
-metadata <- read_csv(here::here('data', 'Camera_operation_years1and2.csv'))
+metadata <- read_csv("~/Dropbox/projects/Hopland - Kaitlyn dissertation/R/hopland-camera-traps/data/camera_operation_phase1.csv")
 
 # get the dates into date format
-metadata[, 2:ncol(metadata)] <- lapply(metadata[, 2:ncol(metadata)], as.Date, format = "%m/%d/%y")
+#metadata[, 2:ncol(metadata)] <- lapply(metadata[, 2:ncol(metadata)], as.Date, format = "%m/%d/%y")
+
+# hardcoding for now; there are lat/long and notes columns that are making this not work as it did for Gorongosa data
+metadata[, 4:7] <- lapply(metadata[, 4:7], as.Date, format = "%m/%d/%y")
 
 records <- left_join(records, metadata) # join by camera
 
@@ -62,27 +58,28 @@ for (i in 1:nrow(records)) {
     records$drop[i] <- FALSE}
 }
 
-summary(records$drop)
+summary(records$drop) # there are 137 (out of 38895)
 
 # exclude records outside of operation dates
 records <- records[records$drop == FALSE,]
 
 # take only columns we want/need
-records <- select(records, Camera, Species, DateTimeOriginal, Date, Time, delta.time.secs, Time.Sun)
+records <- select(records, Camera, Species, Classification, Sex, Age, BuckClass, DateTimeOriginal, Date, Time, delta.time.secs, Time.Sun)
 
-# Merge with species metadata ---------------------------------------------
-
-# bring in species traits
-species <- read_csv(here::here('data', '2018spp_kingdon.csv')) %>%
-  rename(Species = CommName) # rename to match name of column in records
-
-# join records and traits
-records <- left_join(records, species)
-
-# remove those with NA for common name full (this includes setup, ghost, unknown)
-records <- drop_na(records, CommName_Full)
+# NOT RELEVANT FOR HOPLAND DATA
+## Merge with species metadata ---------------------------------------------
+#
+## bring in species traits
+#species <- read_csv(here::here('data', '2018spp_kingdon.csv')) %>%
+#  rename(Species = CommName) # rename to match name of column in records
+#
+## join records and traits
+#records <- left_join(records, species)
+#
+## remove those with NA for common name full (this includes setup, ghost, unknown)
+#records <- drop_na(records, CommName_Full)
 
 
 # Export cleaned file ---------------------------------------------
 
-write_csv(records, here::here('shiny-rai', 'recordtable_allrecordscleaned_speciesmetadata.csv'))
+write_csv(records, here::here('recordtable_hopland_for_shiny.csv'))
